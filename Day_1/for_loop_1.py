4079557a93307227564d129bb4abbfcfde9f5e00
KirbyFerrer <ferrerkirby@rocketmail.com>
2022-06-09 08:00:01 -0700

    save

---
+++ Day_1/for_loop_1.py
@@ -0,0 +1,8 @@
+#Print all integers from 0 to 150.
+for i in range(1, 151):
+    print(i)
+#Print all the multiples of 5 from 5 to 1,000
+for x in range(5, 1005, 5):
+    print(x)
+#Print integers 1 to 100. If divisible by 5, print "Coding" instead. If divisible by 10, print "Coding Dojo".
+for b in range(1, 101):
