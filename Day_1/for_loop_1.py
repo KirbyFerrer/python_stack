69aeb474ca3bb5788a19be4b366e0f026c77aa82
KirbyFerrer <ferrerkirby@rocketmail.com>
2022-06-12 17:19:20 -0700

    for_loop

--- Day_1/for_loop_1.py
+++ Day_1/for_loop_1.py
@@ -1,8 +1,39 @@
 #Print all integers from 0 to 150.
-for i in range(1, 151):
-    print(i)
+#for i in range(1, 151):
+    #print(i)
 #Print all the multiples of 5 from 5 to 1,000
-for x in range(5, 1005, 5):
-    print(x)
+#for x in range(5, 1005, 5):
+    #print(x)
 #Print integers 1 to 100. If divisible by 5, print "Coding" instead. If divisible by 10, print "Coding Dojo".
-for b in range(1, 101)
+#for b in range(1, 101):
+    #if b % 5 == 0:
+        #print('Coding')
+    #if b % 10 == 0:
+        #print('Coding Dojo')
+    #else:
+        #print(b)
+#Add odd integers from 0 to 500,000, and print the final sum.
+#minimum = 0
+#maximum = 500000
+#Oddtotal = 0
+
+#for number in range(minimum, maximum+1):
+    #if(number % 2 != 0):
+        #print("{0}".format(number))
+        #Oddtotal = Oddtotal + number
+
+#print("The Sum of Odd Numbers from {0} to {1} = {2}".format(minimum, maximum, Oddtotal))
+
+#Print positive numbers starting at 2018, counting down by fours
+#number = 2018
+#while number > 0:
+    #print (number)
+    #number = number - 4
+
+#Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
+#def flex_countdown(low, high, mult):
+    #for i in range (low, high):
+        #if i % mult == 0:
+            #print (i)
+            
+#flex_countdown(1, 15, 5)
